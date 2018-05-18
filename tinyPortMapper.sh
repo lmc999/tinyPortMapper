f856558a42268b2ad92af29fa43331c55e4143a1
iiiiiii1 <25471406+iiiiiii1@users.noreply.github.com>
2018-05-18 20:58:13 +0800

    Update and rename tinyPortMapper to tinyPortMapper.sh

--- tinyPortMapper.sh
+++ tinyPortMapper.sh
@@ -70,15 +70,15 @@ config_tinyPortMapper(){
 
 start_tinyPortMapper(){
     echo -e "${Green}正在配置tinyPortMapper...${Font}"
-    nohup ./tinymapper -l 0.0.0.0:${port1} -r ${tinyPortMapperip}:${port2} -t -u > /root/tinymapper.log 2>&1 &
+    nohup /tinyPortMapper/tinymapper -l 0.0.0.0:${port1} -r ${tinyPortMapperip}:${port2} -t -u > /root/tinymapper.log 2>&1 &
     if [ "${OS}" == 'CentOS' ];then
         sed -i '/exit/d' /etc/rc.d/rc.local
-        echo "nohup ./tinymapper -l 0.0.0.0:${port1} -r ${tinyPortMapperip}:${port2} -t -u > /root/tinymapper.log 2>&1 &
+        echo "nohup /tinyPortMapper/tinymapper -l 0.0.0.0:${port1} -r ${tinyPortMapperip}:${port2} -t -u > /root/tinymapper.log 2>&1 &
         " >> /etc/rc.d/rc.local
         chmod +x /etc/rc.d/rc.local
     elif [ -s /etc/rc.local ]; then
         sed -i '/exit/d' /etc/rc.local
-        echo "nohup ./tinymapper -l 0.0.0.0:${port1} -r ${tinyPortMapperip}:${port2} -t -u > /root/tinymapper.log 2>&1 &
+        echo "nohup /tinyPortMapper/tinymapper -l 0.0.0.0:${port1} -r ${tinyPortMapperip}:${port2} -t -u > /root/tinymapper.log 2>&1 &
         " >> /etc/rc.local
         chmod +x /etc/rc.local
     else
@@ -111,7 +111,7 @@ start_tinyPortMapper(){
         #
         # By default this script does nothing.
         " > /etc/rc.local
-        echo "nohup ./tinymapper -l 0.0.0.0:${port1} -r ${tinyPortMapperip}:${port2} -t -u > /root/tinymapper.log 2>&1 &
+        echo "nohup /tinyPortMapper/tinymapper -l 0.0.0.0:${port1} -r ${tinyPortMapperip}:${port2} -t -u > /root/tinymapper.log 2>&1 &
         " >> /etc/rc.local
         chmod +x /etc/rc.local
         systemctl enable rc-local >/dev/null 2>&1
